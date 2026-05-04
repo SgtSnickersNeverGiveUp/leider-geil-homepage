c04987dec5ff7157cfa0f540ed132bc71b560059
SgtSnickersNeverGiveUp <dirkstaerck@web.de>
2026-05-04 05:27:58 +0200

    Refactor supabase client initialization

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,6 @@
-import { supabase } from '../lib/supabase'
 import { createClient } from '@supabase/supabase-js'
 
-// HIER fehlte deine Projekt-ID 'smadmetqgmdukwfskjif' am Anfang!
-const supabaseUrl = 'https://smadmetqgmdukwfskjif.supabase.co' 
-
-const supabaseAnonKey = 'sb_publishable_zclmkpwM2m8g3R35XU6HKw_Un59f87J' 
+const supabaseUrl = 'https://smadmetqgmdukwfskjif.supabase.co'
+const supabaseAnonKey = 'sb_publishable_zclmkpwM2m8g3R35XU6HKw_Un59f87J'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
